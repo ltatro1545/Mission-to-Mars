07ef1ce0e9d4c160ecaaa3cb63c9616e1a0be817
ltatro1545 <92493572+ltatro1545@users.noreply.github.com>
2022-01-01 20:58:12 -0500

    added additional pseudo-code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Import dependencies
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
+# specifically imports scraping.py file
 import scraping
 
 # Flask setup
