9d1a1c36d82643158174252348c27c96bf5f6fa0
ltatro1545 <92493572+ltatro1545@users.noreply.github.com>
2022-01-06 14:35:59 -0500

    added functioning flask app

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    return render_template('index.html', mars=mars)
 
 # Set up scrape route
 @app.route("/scrape")
